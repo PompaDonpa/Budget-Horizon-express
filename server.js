3fdd70804b5aa08cd3ef0be234289fc77923f6cc
Daniel Naranjo <MR.DANIEL.NARANJO@PROTONMAIL.COM>
2021-06-20 15:21:13 -0400

    Dependencies, Configuration and Listener added

---
+++ server.js
@@ -0,0 +1,13 @@
+// Dependencies
+const app =require("./app")
+
+
+// Configuration
+require("dotenv").config()
+const PORT = process.env.PORT || 3001
+
+
+// Listener
+app.listen(PORT, () => { 
+    console.log("Budget-H Listening on PORT : ", PORT)
+ })
