f4a173f350519a221d5b8be5e1bb0105ea79793e
Daniel Naranjo <MR.DANIEL.NARANJO@PROTONMAIL.COM>
2021-06-20 15:20:30 -0400

    Dependencies, Configuration, Middleware, Routes declared

---
+++ app.js
@@ -0,0 +1,35 @@
+// Dependencies
+const express = require("express")
+const cors = require("cors")
+
+
+// Configuration
+const app = express()
+const transactionsController = require('./controllers/transactionController.js')
+
+
+
+// Middleware
+app.use((req, res, next) => { 
+    console.log(`[ Middleware is running! ]`)
+    next()
+})
+app.use(express.json())
+app.use(cors())
+
+
+// Routes 
+
+app.get("/", (req, res) =>{ 
+    res.send('Budget - Horizon')
+ })
+
+ app.use("/transactions", transactionsController)
+
+ app.get("*", (req,res) => { 
+     res.status(404).send('Page not found.')
+  })
+
+
+// Export
+ module.exports = app
